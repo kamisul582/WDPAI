7ddb02170af59b80ccd73fa21ea721bab8a3a8a1
kamil <kamil.sulzycki@op.pl>
2023-05-17 20:30:27 +0200

    database handling

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,11 +2,12 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 class SecurityController extends AppController
 {
     public function login()
     {
-        $user = new User('a', 'a','Jan', 'Kowalski');
+        $userRepository = new UserRepository();
         
         if (!$this->isPost()) {
             return $this->render('login');
@@ -15,6 +16,10 @@ class SecurityController extends AppController
         $email = $_POST['email'];
         $password = $_POST['password'];
 
+        $user = $userRepository->getUser($email);
+        if (!$user) {
+            return $this->render('login', ['messages' => ['User not found!']]);
+        }
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email not exist!']]);
         }
